de9465024f1e91a6f2a6a6cc9fdf8b9807d8e731
Jannatul Ferdous <jannatpinky92@gmail.com>
2022-05-07 21:23:51 -0400

    update button connected with server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion,ObjectId} = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 
@@ -17,58 +17,69 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
-  try{
+  try {
 
     await client.connect();
     const productCollection = client.db('nexus').collection('items');
     //products API
-    app.get('/products',async(req,res)=>{
-        const query={};
-        const cursor =productCollection.find(query);
-        const productsItem=await cursor.toArray();
-        res.send(productsItem);
+    app.get('/products', async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const productsItem = await cursor.toArray();
+      res.send(productsItem);
     });
     //ProductsId api
-    app.get('/update/:id',async(req,res) =>{
-      const id =req.params.id;
-      const query ={_id: ObjectId(id)};
-      const update= await productCollection.findOne(query);
+    app.get('/update/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const update = await productCollection.findOne(query);
       res.send(update);
-  });
-//update product quantity
-  app.put('/quantity/:id', async(req, res) =>{
-    const id = req.params.id;
-    const User = req.body;
-    const filter = {_id: ObjectId(id)};
-    const options = { upsert: true };
-    const updateProductQuantity = {
-        $set: {
-           quantity: user.quantity,
-            sold: user.sold
-        }
-    };
-    const result = await userCollection.updateOne(filter, updatedDoc, options);
-    res.send(result);
+    });
+    //update product quantity
 
-})
 
-//delete from manage inventory page
-app.delete('/update/:id', async(req, res) =>{
-  const id = req.params.id;
-  const query = {_id: ObjectId(id)};
-  const result = await productCollection.deleteOne(query);
-  res.send(result);
-})
- //add new items
- app.post('/products', async (req, res) => {
-  const newService = req.body;
-  const result = await productCollection.insertOne(newService);
-  res.send(result);
-});
-   
+    app.put('/quantity/:id', async (req, res) => {
+      const id = req.params.id;
+      const user = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: { ...user},
+      };
+      const result = await productCollection.updateOne(filter, updateDoc, options);
+      res.send(result)
+    })
+
+    //restock quantity
+    app.put('/restock/:id', async (req, res) => {
+      const id = req.params.id;
+      const user = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: { ...user },
+      };
+      const result = await product.updateOne(filter, updateDoc, options);
+      res.send(result)
+    });
+
+    //delete from manage inventory page
+    app.delete('/update/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    })
+    //add new items
+    app.post('/products', async (req, res) => {
+      const newService = req.body;
+      const result = await productCollection.insertOne(newService);
+      res.send(result);
+    });
+
 
   }
-  finally{
+  finally {
 
   }
 
@@ -80,9 +91,9 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req, res) =>{
-    res.send('Server is running');
+app.get('/', (req, res) => {
+  res.send('Server is running');
 });
-app.listen(port, () =>{
-    console.log('crud is running');
+app.listen(port, () => {
+  console.log('crud is running');
 });
