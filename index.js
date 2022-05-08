5b54ad5ffbe1b3a4a01d18237f65181da9785e1e
Jannatul Ferdous <jannatpinky92@gmail.com>
2022-05-08 09:37:44 -0400

    my items server side done without delete button

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const { send } = require('express/lib/response');
+const { decode } = require('jsonwebtoken');
+
 
 
 const app = express();
@@ -12,6 +16,22 @@ app.use(cors());
 app.use(express.json());
 
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  // console.log(authHeader);
+  if (!authHeader) {
+    return res.status(401).send({ message: 'unauthorized access' })
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access' })
+    }
+    console.log('decoded', decoded);
+    req.decoded = decoded;
+    next();
+  })
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.duvmj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -21,6 +41,16 @@ async function run() {
 
     await client.connect();
     const productCollection = client.db('nexus').collection('items');
+    const orderCollection = client.db('nexus').collection('order');
+
+    //AUTH
+    app.post('/login', async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1d'
+      });
+      res.send({ accessToken });
+    })
     //products API
     app.get('/products', async (req, res) => {
       const query = {};
@@ -35,16 +65,15 @@ async function run() {
       const update = await productCollection.findOne(query);
       res.send(update);
     });
+
     //update product quantity
-
-
     app.put('/quantity/:id', async (req, res) => {
       const id = req.params.id;
       const user = req.body;
       const filter = { _id: ObjectId(id) };
       const options = { upsert: true };
       const updateDoc = {
-        $set: { ...user},
+        $set: { ...user },
       };
       const result = await productCollection.updateOne(filter, updateDoc, options);
       res.send(result)
@@ -59,7 +88,7 @@ async function run() {
       const updateDoc = {
         $set: { ...user },
       };
-      const result = await product.updateOne(filter, updateDoc, options);
+      const result = await productCollection.updateOne(filter, updateDoc, options);
       res.send(result)
     });
 
@@ -70,6 +99,15 @@ async function run() {
       const result = await productCollection.deleteOne(query);
       res.send(result);
     })
+
+    //delete from my item page
+    app.delete('/order/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.send(result);
+    })
+
     //add new items
     app.post('/products', async (req, res) => {
       const newService = req.body;
@@ -77,20 +115,36 @@ async function run() {
       res.send(result);
     });
 
+    //order collection api
+    
+    app.get('/order', verifyJWT, async (req, res) => {
+
+      const decodedEmail = req.decoded.email;
+      const email = req.query.email;
+      if (email === decodedEmail) {
+          const query = { email: email };
+          const cursor = orderCollection.find(query);
+          const orders = await cursor.toArray();
+          res.send(orders)
+      }
+      else {
+          res.status(403).send({ message: 'forbidden access' })
+      }
+  })
+
+  app.post('/order', async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result)
+  })
+
 
   }
   finally {
-
   }
 
 }
 run().catch(console.dir);
-
-
-
-
-
-
 app.get('/', (req, res) => {
   res.send('Server is running');
 });
